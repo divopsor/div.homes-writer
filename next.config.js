36160193cb5ce0b6ed467564674f0ff96f96b852
divopsor <divopsor@gmail.com>
2023-12-31 08:46:57 +0900

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = (phase, { defaultConfig }) => {
   const nextConfig = {
+    trailingSlash: true,
+    ...(process.env.LOCAL === 'true' ? {} : { output: 'export' }),
     // ...defaultConfig,
     reactStrictMode: true,
     swcMinify: true,
@@ -17,6 +19,25 @@ module.exports = (phase, { defaultConfig }) => {
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
     },
+    redirects: process.env.LOCAL === 'true' ? async () => {
+      return [
+        {
+          source: '/',
+          destination: '/writer',
+          permanent: false,
+          basePath: false,
+        }
+      ];
+    }: null,
+    rewrites: process.env.LOCAL === 'true' ? async () => {
+      return [
+        {
+          source: '/api/:slug*',
+          destination: 'https://app.divops.kr/github-api/api/:slug*', // Matched parameters can be used in the destination
+          basePath: false,
+        },
+      ];
+    }: null,
   };
 
   return nextConfig;
